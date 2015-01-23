460edb641cfca8e2967c5c8bd4d23d2afbe72323
Marc-Olivier Fiset <marco.fiset@gmail.com>
2015-01-22 23:37:04 -0500

    Implemented the parser for the new grammar

--- src/Fun/Parsing/Parser.php
+++ src/Fun/Parsing/Parser.php
@@ -3,11 +3,17 @@
 use Exception;
 use Fun\Lexing\Token;
 use Fun\Lexing\TokenType;
-use Fun\Parsing\Nodes\OperationNode;
+use Fun\Parsing\Nodes\ExpressionListNode;
 use Fun\Parsing\Nodes\NumberNode;
+use Fun\Parsing\Nodes\OperationNode;
+use Fun\Parsing\Nodes\VariableAssignmentNode;
+use Fun\Parsing\Nodes\VariableNode;
 
 class Parser
 {
+    /**
+     * @Token[]
+     */
     private $tokens;
 
     public function parse(array $tokens)
@@ -15,34 +21,86 @@ class Parser
         $tokens = $this->filterTokens($tokens);
         $this->tokens = $tokens;
 
-        return $this->parseExpressionNode();
+        return $this->parseExpressionListNode();
     }
 
     private function filterTokens(array $tokens)
     {
-        return array_filter($tokens, function(Token $t) {
+        $filteredTokens = array_filter($tokens, function (Token $t) {
             return $t->getType() !== TokenType::Whitespace;
         });
+
+        return array_values($filteredTokens);
     }
 
+    // ExpressionList = (Expression Terminator)+
+    private function parseExpressionListNode()
+    {
+        $expressions = [];
+
+        while (!$this->isEmpty()) {
+            $expr = $this->parseExpressionNode();
+            $this->expectTokenType(TokenType::Terminator);
+
+            $expressions[] = $expr;
+        }
+
+        return new ExpressionListNode($expressions);
+    }
+
+    // Expression = Assignment | Operation
     private function parseExpressionNode()
     {
-        $left = $this->parseNumberNode();
+        $nextToken = $this->lookAhead();
 
-        if ($this->isEmpty())
+        if ($nextToken->getType() === TokenType::AssignmentOperator) {
+            return $this->parseVariableAssignmentNode();
+        }
+
+        return $this->parseOperationNode();
+    }
+
+    // Assignment = Identifier AssignmentOperator Operation
+    private function parseVariableAssignmentNode()
+    {
+        $identifier = $this->expectTokenType(TokenType::Identifier);
+        $this->expectTokenType(TokenType::AssignmentOperator);
+
+        $operation = $this->parseOperationNode();
+
+        return new VariableAssignmentNode($identifier->getValue(), $operation);
+    }
+
+    // Operation = Term (Operator Term)?
+    private function parseOperationNode()
+    {
+        $left = $this->parseTerm();
+
+        if ($this->isEmpty()) {
             return new OperationNode($left);
+        }
 
         $operatorToken = $this->expectTokenType(TokenType::Operator);
-        $right = $this->parseNumberNode();
+        $right = $this->parseTerm();
 
         return new OperationNode($left, $operatorToken->getValue(), $right);
     }
 
-    private function parseNumberNode()
+    // Term = Idenfitier | Number
+    private function parseTerm()
     {
-        $numberToken = $this->expectTokenType(TokenType::Number);
+        $currentToken = $this->consumeToken();
 
-        return new NumberNode($numberToken->getValue());
+        switch ($currentToken->getType()) {
+            case TokenType::Identifier:
+                return new VariableNode($currentToken->getValue());
+
+            case TokenType::Number:
+                return new NumberNode($currentToken->getValue());
+
+            default:
+                throw new Exception('Expected current token to be a Number or an Identifier.');
+        }
     }
 
     /**
@@ -52,12 +110,44 @@ class Parser
      */
     private function expectTokenType($tokenType)
     {
-        $peek = $this->tokens[0];
+        $peek = $this->currentToken();
 
-        if ($peek->getType() !== $tokenType)
+        if ($peek->getType() !== $tokenType) {
             throw new Exception('Unexpected token type.');
+        }
 
+        return $this->consumeToken();
+    }
+
+    /**
+     * @return Token
+     */
+    private function consumeToken()
+    {
         return array_shift($this->tokens);
+    }
+
+    /**
+     * @return Token
+     * @throws Exception
+     */
+    private function currentToken()
+    {
+        return $this->lookAhead(0);
+    }
+
+    /**
+     * @param int $position
+     * @return Token
+     * @throws Exception
+     */
+    private function lookAhead($position = 1)
+    {
+        if ($position >= count($this->tokens)) {
+            throw new Exception('Tried to look past end of token stream');
+        }
+
+        return $this->tokens[$position];
     }
 
     private function isEmpty()
