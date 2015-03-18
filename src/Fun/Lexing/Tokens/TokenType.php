af8527ee6da631d210bdb0bcfa3c4fe2973cb2f8
Marc-Olivier Fiset <marco.fiset@gmail.com>
2015-03-18 13:00:38 -0400

    Replace double to single quotes for consistency

--- src/Fun/Lexing/Tokens/TokenType.php
+++ src/Fun/Lexing/Tokens/TokenType.php
@@ -2,13 +2,13 @@
 
 final class TokenType
 {
-    const Number = "T_NUMBER";
-    const Operator = "T_OPERATOR";
-    const Whitespace = "T_WHITESPACE";
-    const Identifier = "T_IDENTIFIER";
-    const AssignmentOperator = "T_ASSIGNMENT_OPERATOR";
-    const Terminator = "T_TERMINATOR";
-    const NewLine = "T_NEWLINE";
-    const ConditionalOperator = "T_CONDITIONAL_OPERATOR";
-    const Symbol = "T_SYMBOL";
+    const Number = 'T_NUMBER';
+    const Operator = 'T_OPERATOR';
+    const Whitespace = 'T_WHITESPACE';
+    const Identifier = 'T_IDENTIFIER';
+    const AssignmentOperator = 'T_ASSIGNMENT_OPERATOR';
+    const Terminator = 'T_TERMINATOR';
+    const NewLine = 'T_NEWLINE';
+    const ConditionalOperator = 'T_CONDITIONAL_OPERATOR';
+    const Symbol = 'T_SYMBOL';
 }
